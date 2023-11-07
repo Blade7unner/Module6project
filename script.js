d8921d95b001e647afd704a5295e3ee4c1c994bc
Felipe Benoit <benoit@Felipes-MacBook-Pro.local>
2023-11-06 20:45:35 -0500

    handle the form submission on the homepage

---
+++ script.js
@@ -0,0 +1,9 @@
+//homepage.js
+document.getElementByid("search-form").addEventListener("submit",function (Event) {
+    event.preventDefault() //prevent form submission
+
+    const searchQuery = document.getElementById("searchBox").value;
+    const searchFormat = document.getElementById("typeDrop").value;
+
+    // Redirect 
+}
