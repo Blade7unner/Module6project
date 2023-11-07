768993f2d6b8fe040fdb6e2b0df988dd523036fa
Felipe Benoit <benoit@Felipes-MacBook-Pro.local>
2023-11-06 20:50:58 -0500

    redirect to the search results page with query parameters

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 //homepage.js
 document.getElementByid("search-form").addEventListener("submit",function (Event) {
-    event.preventDefault() //prevent form submission
+    event.preventDefault(); //prevent form submission
 
     const searchQuery = document.getElementById("searchBox").value;
     const searchFormat = document.getElementById("typeDrop").value;
 
-    // Redirect 
-}
+    // Redirect to the search results page with query parameters
+
+    if (searhFormat) {
+        location.replace("`/search-results.html?q=${searchBox}&format=${typeDrop}`");
+
+    } else {
+        location.replace(`/search-results.html?q=${searchBox}`);
+    }
+
+
+});
